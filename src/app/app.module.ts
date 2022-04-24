c3a81866eab0b6a4973dfde15d8079011f80e960
Alejandro Alvarez <alex.david.alvarez.sosa@gmail.com>
2022-04-24 13:14:04 -0400

    Innitial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './sections/banner/banner.component';
+import { TrendingComponent } from './sections/trending/trending.component';
+import { GameCardComponent } from './components/game-card/game-card.component';
+import {NgxFlickingModule} from "@egjs/ngx-flicking";
+import {ReactiveFormsModule} from "@angular/forms";
+import {InfiniteScrollModule} from "ngx-infinite-scroll";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    TrendingComponent,
+    GameCardComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgxFlickingModule,
+        ReactiveFormsModule,
+        InfiniteScrollModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -1,9 +1,6 @@
-import { Injectable } from '@angular/core';
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
-import { environment } from '../../environments/environment';
-import { WorkItem } from '../types/work-item';
-import { Tech } from '../types/tech';
-import { Company } from '../types/company';
+import {Injectable} from '@angular/core';
+import {createClient, SupabaseClient} from '@supabase/supabase-js';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -16,30 +13,32 @@ export class SupabaseService {
   }
 
   private initializeSupabase() {
-    this.supabaseClient = createClient(
-      environment.SUPABASE_URL,
-      environment.SUPABASE_ANON_API_KEY
-    );
+    this.supabaseClient = createClient("https://gqkuommdmfzmwkzdewma.supabase.co",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdxa3VvbW1kbWZ6bXdremRld21hIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDkyNjQyNTIsImV4cCI6MTk2NDg0MDI1Mn0.iF651HDhqynAQRlG8T6wFS3ZEx4dqxHiEiguc0m7-zI", {
+        headers: {
+          Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZWNyZXQiOiJmZjE3ZDE0NmQ4ZTU1OTZjNzMyNzQ1YTEiLCJpYXQiOjE2NTA3NzQ3NjZ9.sdlJx8VSW5zEXGSZeF0KZAUTnua1btj2wwnxDv_kRs8"
+        }
+      });
   }
 
-  getWorks() {
-    return this.supabaseClient
-      .from<WorkItem>('works')
+  getSteam(params = {page: 0, limit: 6, search: '', sortBy: ''}) {
+    const start = params.page * params.limit;
+    const end = start + params.limit - 1;
+    const query = this.supabaseClient
+      .from<any>('steam')
       .select('*')
-      .order('created_at', { ascending: true });
-  }
+      .range(start, end)
+      .limit(params.limit);
 
-  getCompanies() {
-    return this.supabaseClient
-      .from<Company>('companies')
-      .select('*')
-      .order('created_at', { ascending: true });
-  }
 
-  getTechs() {
-    return this.supabaseClient
-      .from<Tech>('techs')
-      .select('*')
-      .order('created_at', { ascending: true });
+    if (params.sortBy) {
+      query.order(params.sortBy)
+    }
+    if (params.search) {
+      query.ilike('title', `%${params.search}%`)
+    }
+    return query;
+
+
   }
 }
